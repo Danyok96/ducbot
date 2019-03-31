b582c49ad596f5b89581f2e51c43c1d21d70625e
Danyok <necron-x@mail.ru>
2019-03-31 21:18:28 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -33,10 +33,10 @@ $app->post('/bot', function() use($app) {
 		case 'message_new':
 
 			$request_params= array(
-				'user_id' => $data->object->peer_id,
+				'user_id' => $data->object->user_id,
 				'message' => 'Тест',
 				'access_tocken' => getenv('VK_TOKEN'),
-				'v' => '5.92'
+				'v' => '5.69'
 			);
 
 			file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
