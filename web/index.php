b8ed7d87c1e66ebf72d6a89e1fd9a9ca590f1b55
Danyok <necron-x@mail.ru>
2019-04-02 12:53:02 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -35,7 +35,7 @@ $app->post('/bot', function() use($app) {
 		case 'message_new':
 			$user_id = $data->object->user_id;
 			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={getenv('VK_TOKEN'))}&v=5.69"));
-			$user_name = $user_info->response->first_name;
+			$user_name = $user_info->response[0]->id;
 			$message = $data->object->body;
 			$messages_array = [
 				'Привет дуц' => "Привет {$user_name}!",
