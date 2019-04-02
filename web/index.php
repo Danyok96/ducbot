a75b4260b944e82d60349de249d808b5c0d7ab7d
Danyok <necron-x@mail.ru>
2019-04-02 11:59:38 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -21,10 +21,10 @@ $app->post('/bot', function() use($app) {
 	$data = json_decode(file_get_contents('php://input'));
 
 	if(!$data)
-		return "nioh";
+		return "miss data";
 
 	if($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
-		return "nioh";
+		return "wrong data";
 
 	switch($data->type)
 	{
@@ -33,26 +33,29 @@ $app->post('/bot', function() use($app) {
 			break;
 
 		case 'message_new':
-			$formula = $data->object->body;
-			$precision = 2; // Number of digits after the decimal point
-
-
-			$request_params = array(
-				'user_id' => $data->object->user_id,
-				'message' => 'Тест',
-				'access_token' => getenv('VK_TOKEN'),
-				'v' => '5.69'
-			);
-			
-			try {
-			    $parser = new FormulaParser($formula, $precision);
-			    $result = $parser->getResult(); // [0 => 'done', 1 => 16.38]
-			    $request_params['message'] = 'Ответ: '. $result['1'];
-			    if($data->object->body == 'ходж'){$request_params['message'] = 'Собака сутулая.=)';};
-			} catch (\Exception $e) {
-			     $request_params['message'] = 'Неа...';
+			$user_id = $data->object->user_id;
+			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.69"));
+			$user_name = $user_info->response[0]->first_name;
+			$message = $data->object->body;
+			$messages_array = [
+				'Привет дуц' => "Привет {$user_name}!",
+				'Дуц, как дела?' => "Збс, ведь я не учусь.=)",
+				'Дуц, что умеешь?' => "Кидать подгоны, чтобы Даня не агрился.=)",
+				'Дуц, кто такой ходж?' => "Нормас чел, иногда сутулы, конечно, но ладно.=)"
+			];
+			foreach ($messages_array as $k => $v) {
+				if ($message == $k) {
+					$otvet = $v;
+				}
 			}
 
+			$request_params = [
+				'user_id' => $user_id,
+				'message' => $otvet,
+				'access_token' => getenv('VK_TOKEN'),
+				'v' => '5.69'
+			];
+			
 			file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
 
 			return 'ok';
