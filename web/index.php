0a81af6d9892a2b588b0e2649861987dc849ccfc
Danyok <necron-x@mail.ru>
2019-04-02 14:36:21 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -68,6 +68,8 @@ $app->post('/bot', function() use($app) {
 			    $parser = new FormulaParser($formula, $precision);
 			    $result = $parser->getResult(); // [0 => 'done', 1 => 16.38]
 			  	if($result['1'] !== 'Syntax error'){$request_params['message'] = 'Ответ: '. $result['1'];};
+			  	if($result['1'] !== 'Invalid character'){$request_params['message'] = 'Ответ: '. $result['1'];};
+			  	
 			    
 			} catch (\Exception $e) {
 			     $request_params['message'] = 'Неа...';
