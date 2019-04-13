74d75998f81fee22fc608ec4faf8c897ed7e0f74
Danyok <necron-x@mail.ru>
2019-04-13 19:10:46 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -283,9 +283,9 @@ $app->post('/bot', function() use($app) {
 								case 'Sat':
 									$otvet = "[id{$user_id}|{$user_name}]{$pref},\n08:30-10:05 МСиСЦОС(лек) 1146л\n10:15-11:50 ЦОС(лек) 1146л\n12:00-13:35 ЧМ(сем) 526";
 									break;
-								}		
-					break;	
+								}	
 						}
+					break;	
 				case 'тест':
 							$otvet = "{$sex}";
 							break;		
