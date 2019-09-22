ca60daa1803f26f0c3956f7a272f0faf9690a60f
Danyok <necron-x@mail.ru>
2019-09-22 15:25:44 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -408,8 +408,7 @@ $app->post('/bot', function() use($app) {
 						$witURL = $witRoot . "v=" . $witVersion ;
 
 						$ch = curl_init();
-						$header = array();
-						$header[] = "Authorization: Bearer QCDW4ADLLIDB3NYO2OTDAPZAOQQXC2BU","Content-Type: audio/mpeg3","{$voice_link_mp}";
+						$header = array("Authorization: Bearer QCDW4ADLLIDB3NYO2OTDAPZAOQQXC2BU","Content-Type: audio/mpeg3","{$voice_link_mp}");
 
 						curl_setopt($ch, CURLOPT_URL, $witURL);
 						curl_setopt($ch, CURLOPT_POST, 1);  //sets method to POST (1 = TRUE)
