79f368e8cb52a53fc0b0104d79588ca5b92ab7ae
Danyok <necron-x@mail.ru>
2019-09-22 16:21:37 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -408,13 +408,13 @@ $app->post('/bot', function() use($app) {
 						$witURL = $witRoot . "v=" . $witVersion ;
 
 						$ch = curl_init();
-						$header = array("Authorization: Bearer QCDW4ADLLIDB3NYO2OTDAPZAOQQXC2BU","Content-Type: audio/ogg","Transfer-Encoding: chunked",'@'."{$voice_link_ogg}");
+						$header = array("Authorization: Bearer QCDW4ADLLIDB3NYO2OTDAPZAOQQXC2BU","Content-Type: audio/ogg","Transfer-Encoding: chunked");
 
 						curl_setopt($ch, CURLOPT_URL, $witURL);
 						curl_setopt($ch, CURLOPT_POST, 1);  //sets method to POST (1 = TRUE)
 						curl_setopt($ch, CURLOPT_HTTPHEADER,$header); //sets the header value above - required for wit.ai authentication
 						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //inhibits the immediate display of the returned data
-						//$body = array();
+						//$body = array('@'."{$voice_link_ogg}");
 						//curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
 						$server_output = curl_exec ($ch); //call the URL and store the data in $server_output
 
