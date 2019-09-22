a32452f4b21053de7530b8a3561bdc85dbc8758e
Danyok <necron-x@mail.ru>
2019-09-22 16:42:27 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -428,7 +428,7 @@ $app->post('/bot', function() use($app) {
 						curl_close($ch);
 						$decodedResponse = json_decode($res, true);
 
-						$otvet = "{$decodedResponse}";
+						$otvet = "{$decodedResponse->_text}";
 
 						//-----
 					} else {
