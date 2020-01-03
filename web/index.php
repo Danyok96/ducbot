cc6423a4ed897b125e2098972b76df1bc85bea43
Daniil <necron-x@mail.ru>
2020-01-03 18:29:50 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -470,7 +470,7 @@ $app->post('/bot', function() use($app) {
 					$otvet = "[id{$user_id}|{$user_name}{$pref}],\nhttps://yadi.sk/d/zkdaamG-Ol-sjg";
 					break;
 				case 'дуцсколькоосталось':
-					$otvet = "Чилить осталось:\n" . time_elapsed_string('2020-01-09 15:00:00', true);
+					$otvet = "[id{$user_id}|{$user_name}{$pref}],\n" . "Чилить осталось:\n" . time_elapsed_string('2020-01-09 15:00:00', true);
 					break;
 				case 'дуцссылканапрезентации':
 					$otvet = "[id{$user_id}|{$user_name}{$pref}],\nСотСС:\nhttps://cloud.mail.ru/public/cgkk/48CYfcwtt\nТОР:\nhttps://cloud.mail.ru/public/4rtR/3agzjK72Z\nТПРС:\nhttps://cloud.mail.ru/public/wGCj/3xfHLB5AZ";
