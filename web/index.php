a5d7934d6c9444a844bdd892480a267385e353f5
Daniil <necron-x@mail.ru>
2020-01-13 02:11:23 +0300

    UPD

--- web/index.php
+++ web/index.php
@@ -479,12 +479,12 @@ $app->post('/bot', function() use($app) {
 					$otvet = "[id{$user_id}|{$user_name}{$pref}],\nСчётчик дуца: {$mytext}";
 					break;
 					//------------
-					case 'дуцсоздайфайл':
-					$fp = fopen("counter.txt", "w+"); // Открываем файл в режиме записи
-					$mytext = 0; // Исходная строка
-					$test = fwrite($fp, $mytext); // Запись в файл
-					fclose($fp); //Закрытие файла
-						break;
+					// case 'дуцсоздайфайл':
+					// $fp = fopen("counter.txt", "w+"); // Открываем файл в режиме записи
+					// $mytext = 0; // Исходная строка
+					// $test = fwrite($fp, $mytext); // Запись в файл
+					// fclose($fp); //Закрытие файла
+					// 	break;
 					//------------
 				// case 'дуцчтениефайла':
 				// 	$fp = fopen("counter.txt", "r"); // Открываем файл в режиме чтения
