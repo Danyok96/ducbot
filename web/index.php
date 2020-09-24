2a166f852615bc647ad3f1869df3b2f0f97be2be
Danyok <necron-x@mail.ru>
2020-09-24 21:23:07 +0300

    convert

--- web/index.php
+++ web/index.php
@@ -644,7 +644,7 @@ $app->post('/bot', function() use($app) {
 			}
 			//---------------------------------------------------
 			//конвертация величин
-			if(strpos($message_to_convert, 'дуцпереведи') !== false)
+			if(strpos($message_to_convert, 'дуц переведи') !== false)
 			{
 				$message_to_convert = str_replace('дуцпереведи', '', $message_to_convert);
 				$otvet = $message_to_convert;
