36dcc8c8257c8ac8d2c90acc5758fd3fa169d337
Danyok <necron-x@mail.ru>
2020-09-24 22:03:01 +0300

    convert

--- web/index.php
+++ web/index.php
@@ -651,7 +651,7 @@ $app->post('/bot', function() use($app) {
 				$pos_b = strrpos($message_to_convert, ' в ');
 				$first_part = mb_substr( $message_to_convert, 0, $pos_a);
 				$second_part = mb_substr( $message_to_convert, $pos_b);
-				$first_part = str_replace(' в ', '', $first_part);
+				//$first_part = str_replace(' в ', '', $first_part);
 				$otvet = "Строка: ".$message_to_convert."\nПервая часть: ".$first_part."\nВторая часть: ".$second_part;
 
 			}
